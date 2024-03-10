c1a88b2c616c9952f02f252d2c708eddc8df3e7f
Roby <roberto.g.mellone@gmail.com>
2024-03-10 22:10:51 +0100

    installazione funzionante di django messa nella cartella superiore

--- django_rust/guestbook/settings.py
+++ django_rust/guestbook/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-*m4c%gq(jw*=vx(bj0k7x+h7mmb8fe^1z@#hxch!ixq96lpr#y"
+SECRET_KEY = "django-insecure-i)y-&g+sj2olyzj8nlep&5ure)tx+tzv=nn=5y^log_ft4-zd$"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "notes.apps.NotesConfig"
+    "notes.apps.NotesConfig",
 ]
 
 MIDDLEWARE = [
